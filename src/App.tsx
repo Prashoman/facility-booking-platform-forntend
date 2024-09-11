0f251cef301ea28723f79e5f0b78a4bff9e13570
Prashoman <badhonchakrabarti100823>
2024-09-12 00:03:58 +0600

    inisital project and router and tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "./components/shared/Navbar/Navbar"
+import Footer from "./components/shared/Footer/Footer"
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
